a8a4fe2a59469f426c45565385e89df4325d9783
Nercino Neto <njbneto21@gmail.com>
2022-11-29 19:05:00 -0300

    feat(app/scripts/time_bar_data.py): :sparkles: Convert from minutes to seconds
    Creating function: date_in_seconds.
    Get data that is in minutes (study_time,
    break_time, long_break_time) and multiply                                                                                                  by 60, turning it into seconds.

--- app/scripts/time_bar_data.py
+++ app/scripts/time_bar_data.py
@@ -15,3 +15,9 @@ class TimerData:
     study_time: int
     break_time: int
     long_break_time: int
+
+    def date_in_seconds(self) -> list[int] :
+
+        return [self.study_time * 60,
+                self.break_time * 60,
+                self.long_break_time * 60]
