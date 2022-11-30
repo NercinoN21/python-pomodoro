ab769113a14dfecc10346dd0f9d1c841ed9f9f9e
Nercino Neto <njbneto21@gmail.com>
2022-11-29 21:47:12 -0300

    feat(app/scripts/user_input.py): :sparkles: Getting user data
    Creating function: setting_time.
    Creates a window where the user will
    go decide the study time and breaks.

--- app/scripts/user_input.py
+++ app/scripts/user_input.py
@@ -10,3 +10,37 @@ import PySimpleGUI as sg
 
 # Setting logo
 from main import *
+
+
+def setting_time() -> dict:
+    layout = [
+        [sg.Image(logo)],
+        [sg.Text('Enter the time in minutes:')],
+        [
+            sg.Text('Study ', size=(13, 1)), 
+            sg.InputText(size=(10, 1))
+        ],
+        [
+            sg.Text('Break Time', size=(13, 1)), 
+            sg.InputText(size=(10, 1))
+        ],
+        [
+            sg.Text('Long Break Time ', size=(13, 1)), 
+            sg.InputText(size=(10, 1))
+        ],
+        [sg.Submit(), sg.Cancel()]
+    ]
+
+    # Invoking window
+    window = sg.Window('Setting Timer', layout)
+
+    # Capturing outputs
+    submit, values_time = window.read()
+
+    # Checking if the user has submitted the values
+    if submit != 'Submit':
+        return {-1: 'exit'}
+
+    window.close()
+    
+    return values_time
