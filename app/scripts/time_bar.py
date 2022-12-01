f53545a4f28a746aa370996bbc4fbc41808e1fbb
Nercino Neto <njbneto21@gmail.com>
2022-12-01 19:51:13 -0300

    feat(app/scripts/time_bar.py): :sparkles: Running time bar
    Creating function: run_time_bar.
    Responsible for summoning and
    operating the time bar.

--- app/scripts/time_bar.py
+++ app/scripts/time_bar.py
@@ -16,3 +16,39 @@ from playsound import playsound
 
 # Setting logo and alarm
 from main import logo, alarm
+
+
+def run_time_bar(max_seconds: int, title: str):
+    layout = [[sg.Image(logo)],
+              [sg.Text('In progress...')],
+              [sg.ProgressBar(max_value=max_seconds, orientation='h', 
+                    size=(23, 30), key='_loading_', style='classic')]
+    ]
+
+    # Invoking window
+    window = sg.Window(title, layout)
+
+    # Window to update
+    refresh_window = window['_loading_']
+
+    current_value = 0
+    while True:
+        # Capturing output
+        event = window.read(timeout=100)[0]
+    
+        # If press the "x" close the window
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            window.close()
+            break
+        else:
+            current_value+=1
+
+            # Ring the alarm and close the window
+            if current_value > max_seconds:
+                playsound(alarm)
+                window.close()
+                break
+            
+            # Waiting 1 second and updating the bar
+            time.sleep(1)
+            refresh_window.UpdateBar(current_value)
