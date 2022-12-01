37a74f29e338d63815809c535ef37896f4027fcf
Nercino Neto <njbneto21@gmail.com>
2022-12-01 20:37:45 -0300

    feat(app/scripts/pomodoro.py): :sparkles: Defining how the pomodoro works
    Responsible for run the study
    time, break and final message.

--- app/scripts/pomodoro.py
+++ app/scripts/pomodoro.py
@@ -11,3 +11,32 @@ from user_input import *
 from pop_pups import *
 from time_bar import *
 from time_bar_data import TimeData
+
+
+def pomodoro(data: TimeData):
+
+    # Setting time in seconds
+    sec_study, sec_break, sec_long_break = data.date_in_seconds()
+
+    number_sessions = 0
+    while True:
+        # Starting study time
+        run_time_bar(sec_study, title='Studying...')
+
+        # Storing user option
+        option = echoose_break_time(data.break_time, 
+                                    data.long_break_time)
+        # Checking option
+        match option:
+            case 1:
+                run_time_bar(sec_break, title='Break Time...')
+            case 2:
+                run_time_bar(sec_long_break, title='Long Break Time...')
+            case -1:
+                break
+        
+        # Showing total sessions
+        number_sessions+=1
+        sessions(number_sessions)
+
+    end()
