b9b54beb9d32cb8977e72b491671c4794d4c47f9
Nercino Neto <njbneto21@gmail.com>
2022-12-01 20:26:16 -0300

    feat(app/scripts/pomodoro.py): :sparkles: Importing into the pomodoro function

--- app/scripts/pomodoro.py
+++ app/scripts/pomodoro.py
@@ -4,3 +4,10 @@
 
     @author Nercino Neto
 '''
+
+
+# Importing functions needed to create the Pomodoro
+from user_input import *
+from pop_pups import *
+from time_bar import *
+from time_bar_data import TimeData
