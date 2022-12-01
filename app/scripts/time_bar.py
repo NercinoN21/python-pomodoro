8d8cc6c330e1b8518d97ee0d6ba411d17bf1e8ff
Nercino Neto <njbneto21@gmail.com>
2022-12-01 19:33:16 -0300

    feat(app/scripts/time_bar.py): :sparkles: Importing necessary things
    time: To sleep.
    PysimpleGUI: To create windows.
    playsound: To play the alarm.
    main: To use the variable logo and alarm.

--- app/scripts/time_bar.py
+++ app/scripts/time_bar.py
@@ -3,3 +3,16 @@
 
     @author Nercino Neto
 '''
+
+
+# Native
+import time
+
+# pip install pysimplegui
+import PySimpleGUI as sg
+
+# I recommend pip install playsound==1.2.2
+from playsound import playsound 
+
+# Setting logo and alarm
+from main import logo, alarm
