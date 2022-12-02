0d0d01c52c0359f9ad6f108e7b5c01abb2445979
Nercino Neto <njbneto21@gmail.com>
2022-12-01 21:13:03 -0300

    feat(app/scripts/main.py): :sparkles: Creating main function
    Creating function: main.
    Applying operating rule,
    getting data and checking it,
    giving a correct output for each data.

--- app/scripts/main.py
+++ app/scripts/main.py
@@ -13,3 +13,32 @@ from pop_pups import *
 
 # To apply the theme
 from PySimpleGUI import theme
+
+
+def main():
+
+    theme('Black')
+
+    # Defining data
+    datebase = setting_time()
+
+    if checking_data(datebase):
+        # Convert to int
+        int_datebase = turning_data_into_int(datebase)
+
+        # Creating class
+        final_datebase = TimeData(int_datebase[0], 
+                                  int_datebase[1],
+                                  int_datebase[2])
+        # Invoking pomodoro
+        pomodoro(final_datebase)
+    elif datebase == {-1: 'exit'}:
+        ''' Just leave '''
+        pass
+    else:
+        # Orienting and redirecting
+        error()
+        main()
+
+if __name__ == '__main__':   
+    main()
