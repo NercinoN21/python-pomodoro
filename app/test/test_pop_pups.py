13466f7d54f23cb3e5689f188a9898a695439d70
Nercino Neto <njbneto21@gmail.com>
2022-12-05 18:38:57 -0300

    feat(app/test/test_pop_pups.py): :sparkles: Importing necessary things

--- app/test/test_pop_pups.py
+++ app/test/test_pop_pups.py
@@ -3,3 +3,25 @@
 
     @author Nercino Neto
 '''
+
+
+# Native
+import sys
+import os
+
+import colorama
+colorama.init()
+from colorama import Fore
+
+# Getting the scripts module
+sys.path.insert(0,os.path.abspath(
+                    os.path.join(
+                        os.path.dirname(__file__), '..')))
+import scripts.pop_pups as pp
+
+# For the tests(Native)
+import unittest
+
+# pip install pysimplegui
+# To set the theme
+from PySimpleGUI import theme
