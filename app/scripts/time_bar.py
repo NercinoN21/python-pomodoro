89004e7fb73d3abde839832c745ef9a63bbb0e67
Nercino Neto <njbneto21@gmail.com>
2022-12-16 21:15:18 -0300

    refactor(app/scripts/time_bar.py): :recycle: Restructuring how the time bar window works
    Separating the "run_time_bar"
    function in 3 and changing loop.
    
    creat_time_bar: Structure of the window.
    loop_time_bar: Update window value.
    run_time_bar: Run the functions.

--- app/scripts/time_bar.py
+++ app/scripts/time_bar.py
@@ -19,8 +19,66 @@ logo = "app/media/logo.png"
 alarm = "app/media/alarm.mp3"
 
 
+def creat_time_bar(max_seconds: int, title: str) -> sg.Window:
+    """Creating time bar window structure
+
+    Args:
+        max_seconds (int): Execution time
+        title (str): Text that will be in 
+                     the header of the page
+
+    Returns:
+        sg.Window: To update bar value and close window
+    """
+
+    # Window structure
+    layout = [
+        [sg.Image(logo)],
+        [sg.Text('In progress...')],
+        [sg.ProgressBar(max_value=max_seconds, 
+                        orientation='h', 
+                        size=(23, 30), 
+                        key='_loading_', 
+                        style='classic')]
+    ]
+
+    # Invoking window
+    window = sg.Window(title, layout)
+
+    return window
+
+def loop_time_bar(window: sg.Window, max_seconds: int) -> bool:
+    """Responsible for updating the time bar
+
+    Args:
+        window (sg.Window): To update bar value and close window
+        max_seconds (int): Execution time
+
+    Returns:
+        bool: If the time is completed, it returns true, otherwise false
+    """
+
+    for second in range(max_seconds):
+        # Capturing output
+        event = window.read(timeout=100)[0]
+
+        # If press the "x" close the window
+        if event == sg.WIN_CLOSED:
+            return False
+
+        # Waiting a second considering processing time
+        time.sleep(0.89)
+
+        # updating the bar
+        window['_loading_'].UpdateBar(second+1)
+
+    return True
+
 def run_time_bar(max_seconds: int, title: str):
     """General operation of the time bar
+
+       Initializing the window, ringing the alarm when 
+       the time is up(or not) and closing the window
 
     Args:
         max_seconds (int): Execution time
@@ -28,36 +86,15 @@ def run_time_bar(max_seconds: int, title: str):
                      the header of the page
     """
 
-    layout = [[sg.Image(logo)],
-              [sg.Text('In progress...')],
-              [sg.ProgressBar(max_value=max_seconds, orientation='h', 
-                    size=(23, 30), key='_loading_', style='classic')]
-    ]
+    # Generating time bar
+    windons = creat_time_bar(max_seconds, title)
 
-    # Invoking window
-    window = sg.Window(title, layout)
-
-    # Window to update
-    refresh_window = window['_loading_']
-
-    current_value = 0
-    while True:
-        # Capturing output
-        event = window.read(timeout=100)[0]
-    
-        # If press the "x" close the window
-        if event == sg.WIN_CLOSED or event == 'Exit':
-            window.close()
-            break
-        else:
-            current_value+=1
-
-            # Ring the alarm and close the window
-            if current_value > max_seconds:
-                playsound(alarm)
-                window.close()
-                break
-            
-            # Waiting 1 second and updating the bar
-            time.sleep(1)
-            refresh_window.UpdateBar(current_value)
+    # If: 
+    #   The user does not interrupt the window
+    # Else:
+    #   The user interrupts the window
+    if loop_time_bar(windons, max_seconds):
+        playsound(alarm)
+        windons.close()
+    else:
+        windons.close()
